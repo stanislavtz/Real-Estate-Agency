9104ba932e5294ae0db04e723d418cd2cff032bc
Tzekov <stanislavtz@gmail.com>
2021-10-25 12:47:54 +0300

    Add housing routing

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 
 const homePageController = require('./controllers/homePage');
 const usersController = require('./controllers/usersController');
+const offersController = require('./controllers/offersController');
 
 router.use('/', homePageController);
 router.use('/users', usersController);
+router.use('/housing', offersController);
 
 router.all('*', (req, res) => res.render('404', {title: 'Page Not Found'}));
 
