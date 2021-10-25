ab982f0a0b649da19baea5220233170093965252
Tzekov <stanislavtz@gmail.com>
2021-10-25 19:54:39 +0300

    Fix uncorrect named constant

--- src/controllers/homePage.js
+++ src/controllers/homePage.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 const offersService = require('../services/offersService');
 
-const topLevel = 3;
+const numberOfOffers = 3;
 
 async function getHomePage(req, res) {
     res.locals.title = 'Home Page';
     try {
-        const offers = await offersService.getAll(topLevel);
+        const offers = await offersService.getAll(numberOfOffers);
 
         res.render('home/index', { offers });
     } catch (error) {
