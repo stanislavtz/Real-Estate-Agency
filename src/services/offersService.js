f9014854f0575b9f605d4f41c45735ac3a442a82
Tzekov <stanislavtz@gmail.com>
2021-10-25 19:56:14 +0300

    Remove empty line

--- src/services/offersService.js
+++ src/services/offersService.js
@@ -3,7 +3,6 @@ const Housing = require('../models/Housing');
 const getAll = (params) => 
     params ? Housing.find().limit(params).sort({ createdAt: -1 }).lean() : Housing.find().lean();
 
-
 const create = (data) => Housing.create(data);
 
 const getOne = (id) => Housing.findById(id).populate('tenants').lean();
