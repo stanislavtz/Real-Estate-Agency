770237d4f5482144499988a2fe93f98f26433a8c
Tzekov <stanislavtz@gmail.com>
2021-10-25 19:26:14 +0300

    Add runValidators to update function

--- src/services/offersService.js
+++ src/services/offersService.js
@@ -1,18 +1,18 @@
 const Housing = require('../models/Housing');
 
-const create = (data) => Housing.create(data);
-
 const getAll = (params) => {
-    if(params) {
+    if (params) {
         return Housing.find().limit(params).sort({ createdAt: -1 }).lean();
     }
 
     return Housing.find().lean();
 }
 
+const create = (data) => Housing.create(data);
+
 const getOne = (id) => Housing.findById(id).populate('tenants').lean();
 
-const update = (id, offer) => Housing.findByIdAndUpdate(id, offer);
+const update = (id, offer) => Housing.findByIdAndUpdate(id, offer, { runValidators: true });
 
 module.exports = {
     create,
